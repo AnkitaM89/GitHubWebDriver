8ffc19ea99cb887963bcfa1994a0cf48dba1fd85
Ankita <Ankita@192.168.0.104>
2023-09-11 23:51:14 +0530

    Created classes to demonstrate DragandDrop and Drag andDropBy methods of Action class

---
+++ Webdriver_Project/src/qedge/aug2/DragDrop2.java
@@ -0,0 +1,30 @@
+package qedge.aug2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class DragDrop2 {
+
+	public static void main(String[] args) throws Throwable {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://jqueryui.com/droppable/");
+		Thread.sleep(2000);
+		Actions ac = new Actions(driver);
+		//switch to frame
+		driver.switchTo().frame(0);
+		WebElement source = driver.findElement(By.id("draggable"));
+		WebElement target = driver.findElement(By.id("droppable"));
+		int x =target.getLocation().getX();
+		int y =target.getLocation().getY();
+		System.out.println(x+"      "+y);
+		ac.dragAndDropBy(source, x, y).perform();
+		Thread.sleep(2000);
+		driver.quit();
+	}
+
+}
